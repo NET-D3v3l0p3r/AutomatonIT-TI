73239bf53921e3af46825c6fb4631491e0c25094
NET-D3v3l0p3r <3r0rxx@gmail.com>
2018-10-09 23:22:36 +0200

    Update Program.cs

--- Automaton/Program.cs
+++ Automaton/Program.cs
@@ -10,12 +10,6 @@ namespace Automaton
     {
         static void Main(string[] args)
         {
-            Automaton<int, int> a = new Automaton<int, int>(new int[] { 50, 100, 200 }, new int[] { 0, 50, 100, 150, 200 }, 0, new int[] { 200 });
-            a.CreateStates((x, y) => (x + y) >= 200 ? 200 : (x + y));
-
-            Console.WriteLine(a.Evaluate(new int[] { 200, 50, 100 }));
-
-
             Automaton<char, bool> automaton = new Automaton<char, bool>(new char[] { 'A', 'B' }, new bool[] { true, false }, true, new bool[] { true });
             automaton.CreateStates((x, y) => x == 'A' ? (y = !y) : y);
 
